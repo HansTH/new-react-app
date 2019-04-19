d11ec511ec46691b6183807ba26ffe3dff306fa1
HansTH <hbhorst@mac.com>
2019-04-19 14:39:26 +0200

    setup React and ReactDOM.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 import './main.css';
-alert('hello webpack');
+
+ReactDOM.render(<App />, document.getElementById('root'));
