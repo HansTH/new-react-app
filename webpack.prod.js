1f8ab59514d55117c922307c306c7db54edd5c1a
HansTH <hbhorst@mac.com>
2019-04-19 12:18:28 +0200

    add and setup terser-webpack-plugin for minify main.js.

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,6 +4,7 @@ const merge = require('webpack-merge');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: 'production',
@@ -26,6 +27,6 @@ module.exports = merge(common, {
 		})
 	],
 	optimization: {
-		minimizer: [new OptimizeCssAssetsPlugin()]
+		minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()]
 	}
 });
